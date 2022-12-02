ac8ca76e78fc271d509c8da0f1611019e833acd1
Corentinmiserque <113233819+Corentinmiserque@users.noreply.github.com>
2022-12-02 20:11:50 +0100

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,36 @@
-# collection-project1
+# collection-project
+
+/\*
+header {
+display: flex;
+background-color:black;
+img{
+width: 25vw;
+margin: 2.5vw 5vw;
+}
+nav{
+
+        ul{
+            position: absolute;
+            top: 10vw;
+            left: 38vw;
+            li{
+                display: inline;
+                justify-content: space-around;
+                font-size: 3vw;
+                margin-right: 3vw;
+                transition: 0.1s;
+                &:hover{
+                    color: #7f7d7d;
+                    transition: 0.1s;
+                }
+            }
+        }
+
+
+
+    }
+
+}
+
+divNumber.style.backgroundImage = "url(" + collectionSerie[i]["picture"] +");";
---
+++ script.js
@@ -0,0 +1,220 @@
+const collectionSerie = [
+    {
+        name: "Elite",
+        director:"Darío Madrona",
+        years:"2018",
+        picture: "https://images.critictoo.com/wp-content/uploads/2018/09/Elite-Saison-1.jpg",
+        genre: "Suspense",
+        summary:"Las Encinas est une école privée huppée d’Espagne réservée aux enfants de l’élite. Mais quand leur lycée est détruit, trois adolescents issus de la classe populaire y sont intégrés. Cependant, ils vont vite regretter cette opportunité... ",
+        logo:"https://upload.wikimedia.org/wikipedia/commons/thumb/5/54/Netflix_logo.png/800px-Netflix_logo.png",
+        link:"https://www.netflix.com/browse",
+    },
+    {
+        name: "Warior Run",
+        director:"Simon Barry",
+        years:"2020",
+        picture: "https://fr.web.img3.acsta.net/pictures/20/06/18/09/35/0755176.jpg",
+        genre: "Action",
+        summary:"Une jeune femme d'une vingtaine d'années se réveille à la morgue, vivante, avec un artefact implanté dans le dos. Elle découvre qu'elle est maintenant membre d'un ordre ancien qui s'est donné pour mission de combattre les démons sur Terre.",
+        logo:"https://upload.wikimedia.org/wikipedia/commons/thumb/5/54/Netflix_logo.png/800px-Netflix_logo.png",
+        link:"https://www.netflix.com/browse",
+    },
+    {
+        name: "Blood and water",
+        director:"Gambit Films",
+        years:"2020",
+        picture: "https://www.bellanaija.com/wp-content/uploads/2020/05/Blood-and-Water-2-750x600.jpeg",
+        genre: "Suspense",
+        summary:"Une adolescente obtient son transfert dans le même lycée qu'une jeune fille qu'elle soupçonne d'être sa soeur, enlevée à la naissance 17 ans auparavant.",
+        logo:"https://upload.wikimedia.org/wikipedia/commons/thumb/5/54/Netflix_logo.png/800px-Netflix_logo.png",
+        link:"https://www.netflix.com/browse",
+    },
+    {
+        name: "IZombie",
+        director:"Rob Thomas",
+        years:"2015",
+        picture: "https://fr.web.img5.acsta.net/pictures/18/02/19/15/23/4086911.jpg",
+        genre: "Policier",
+        summary:"Olivia Moore, surnommée Liv, une étudiante transformée en zombie lors d'une soirée qui a très mal tourné, travaille en tant que médecin légiste afin de pouvoir profiter du festin que représentent pour elle les cervelles des défunts. ",
+        logo:"https://upload.wikimedia.org/wikipedia/commons/thumb/5/54/Netflix_logo.png/800px-Netflix_logo.png",
+        link:"https://www.netflix.com/browse",
+    },
+    {
+        name: "Alice in Borderland",
+        director:"Tsuyoshi Imai",
+        years:"2020",
+        picture:"https://www.nautiljon.com/images/drama/00/77/imawa_no_kuni_no_alice_4477.jpg",
+        genre: "Fantastique",
+        summary:"Un jeune homme passionné de jeux vidéo se retrouve avec ses deux amis dans un Tokyo alternatif où ils doivent disputer de dangereuses parties pour survivre.",
+        logo:"https://upload.wikimedia.org/wikipedia/commons/thumb/5/54/Netflix_logo.png/800px-Netflix_logo.png",
+        link:"https://www.netflix.com/browse",
+    },
+    {
+        name: "RuPaul's Drag Race ",
+        director:"Nick Murray",
+        years:"2009",
+        picture: "https://ichef.bbci.co.uk/images/ic/1200x675/p07p5h4s.jpg",
+        genre: "Humoristique",
+        summary:"L'émission est un concours de drag queens au cours duquel RuPaul, qui est à la fois présentatrice, mentor et source d'inspiration pour les candidates, sélectionne la « prochaine superstar américaine du drag.",
+        logo:"https://upload.wikimedia.org/wikipedia/commons/thumb/5/54/Netflix_logo.png/800px-Netflix_logo.png",
+        link:"https://www.netflix.com/browse",
+    },
+    {
+        name: "The Flash",
+        director:"Greg Berlanti",
+        years:"2014",
+        picture: "https://flxt.tmsimg.com/assets/p10781465_b_v13_ay.jpg",
+        genre: "Super-Hero",
+        summary:"Jeune expert de la police scientifique de Central City, Barry Allen se retrouve doté d'une vitesse extraordinaire après avoir été frappé par la foudre. Sous le costume de Flash, il utilise ses nouveaux pouvoirs pour combattre le crime.",
+        logo:"https://upload.wikimedia.org/wikipedia/commons/thumb/5/54/Netflix_logo.png/800px-Netflix_logo.png",
+        link:"https://www.netflix.com/browse",
+    },
+    {
+        name: "The Blacklist",
+        director:"Jon Bokenkamp",
+        years:"2013",
+        picture: "https://fr.web.img5.acsta.net/pictures/17/08/30/11/50/532007.jpg",
+        genre: "Thriller",
+        summary:"Le criminel le plus recherché du monde se rend mystérieusement à la police et offre ses services en dénonçant tous ceux avec qui il a collaboré sur ses crimes par le passé. Sa seule contrepartie : travailler avec une agent du FBI débutante avec qui il n'a semble-t-il aucune connexion...",
+        logo:"https://upload.wikimedia.org/wikipedia/commons/thumb/5/54/Netflix_logo.png/800px-Netflix_logo.png",
+        link:"https://www.netflix.com/browse",
+    },
+    {
+        name: "Bonding",
+        director:"Rightor Doyle",
+        years:"2019",
+        picture: "https://fr.web.img2.acsta.net/pictures/20/02/25/06/36/4522700.jpg",
+        genre: "Comédie",
+        summary:"Deux amis inséparables au lycée se retrouvent à New York après s'être perdus de vue, mais leur nouveau terrain de jeu est radicalement différent.",
+        logo:"https://upload.wikimedia.org/wikipedia/commons/thumb/5/54/Netflix_logo.png/800px-Netflix_logo.png",
+        link:"https://www.netflix.com/browse",
+    },
+    {
+        name: "Superstore",
+        director:"Justin Spitzer",
+        years:"2015",
+        picture: "https://fr.web.img6.acsta.net/pictures/18/08/14/16/37/4771722.jpg",
+        genre: "Comédie",
+        summary:"Les drôles de mésaventures des employés d'un grand magasin. Des débutants aux yeux qui brillent aux vétérans ayant déjà tout vécu, en passant par les employés d'été paumés et les managers déterminés, ils affrontent 'en famille' les soucis du quotidien inhérents à la gestion d'une grande surface.",
+        logo:"https://upload.wikimedia.org/wikipedia/commons/thumb/5/54/Netflix_logo.png/800px-Netflix_logo.png",
+        link:"https://www.netflix.com/browse",
+    },
+    {
+        name: "SuperGirl ",
+        director:"Greg Berlanti",
+        years:"2015",
+        picture: "https://fr.web.img5.acsta.net/pictures/18/04/10/10/27/5800369.jpg",
+        genre: "Super Hero",
+        summary:"Les aventures de Supergirl. A 24 ans, elle mène sa vie en marge de celle de son célèbre cousin, se sent impuissante face à ses pouvoirs extraordinaires qu'on lui a appris à ne pas utiliser. Elle va se retrouver à mener une double vie pour combattre le crime dans sa ville.",
+        logo:"https://upload.wikimedia.org/wikipedia/commons/thumb/5/54/Netflix_logo.png/800px-Netflix_logo.png",
+        link:"https://www.netflix.com/browse",
+    },
+    {
+        name: "C'est du gateau ! ",
+        director:"Nicole Byer",
+        years:"2018",
+        picture: "https://images.justwatch.com/poster/97646440/s592/cest-du-gateau",
+        genre: "Humoristique",
+        summary:"De piètres pâtissiers amateurs tentent de reproduire des pâtisseries à l'esthétique complexe, dont la réalisation tient souvent plus de la sculpture que de la simple pâtisserie. Une mission quasi-impossible pour les candidats..",
+        logo:"https://upload.wikimedia.org/wikipedia/commons/thumb/5/54/Netflix_logo.png/800px-Netflix_logo.png",
+        link:"https://www.netflix.com/browse",
+    },
+]
+ 
+function onload (){
+
+//correction bug body 
+
+let newbody = document.getElementsByTagName("body")[0];
+
+ //creation h1
+
+ let h1 = document.createElement("h1");
+ h1.classList.add("h1");
+ newbody.appendChild(h1);
+ h1.textContent= "Séries les plus populaires:";
+
+//création div parent
+
+let parent = document.createElement("div");
+parent.classList.add("parent") ;
+newbody.appendChild(parent);
+   
+//création div enfants
+
+    let divNumber
+
+    for( i = 0; i<=12;i++){
+        divNumber = document.createElement("div");
+        divNumber.classList.add("div","div"+(i+1));
+        divNumber.style.backgroundImage = "url(" + collectionSerie[i]["picture"] + ")";
+        parent.appendChild(divNumber);
+       
+
+//insertion logo netflix 
+
+        let logoimg = document.createElement("img");
+        logoimg.classList.add("logo");
+        divNumber.appendChild(logoimg);
+        logoimg.setAttribute("src","https://upload.wikimedia.org/wikipedia/commons/thumb/5/54/Netflix_logo.png/800px-Netflix_logo.png");
+    
+//creation div invisible
+
+        let invisible = document.createElement("div");
+        invisible.classList.add("invisible");
+        divNumber.appendChild(invisible);
+
+//creation h3
+
+        let h3 = document.createElement("h3");
+        h3.classList.add("genre");
+        invisible.appendChild(h3);
+        h3.textContent= collectionSerie[i]["genre"];
+
+// creation h2
+
+        let h2 = document.createElement("h2");
+        h2.classList.add("name");
+        invisible.appendChild(h2);
+        h2.textContent= collectionSerie[i]["name"];
+
+//insertion span h2
+        let span = document.createElement("span");
+        span.classList.add("years");
+        h2.appendChild(span);
+        span.textContent= collectionSerie[i]["years"];
+
+// creation h4
+
+        let h4 = document.createElement("h4");
+        h4.classList.add("director");
+        invisible.appendChild(h4);
+        h4.textContent= collectionSerie[i]["director"];
+
+// creation p
+
+        let para = document.createElement("p");
+        para.classList.add("summary");
+        invisible.appendChild(para);
+        para.textContent= collectionSerie[i]["summary"];
+        
+// creation button
+        let button = document.createElement("a");
+        button.classList.add("link");
+        invisible.appendChild(button);
+        button.setAttribute("href", collectionSerie[i]["link"]);
+        button.textContent= "Visionner";
+    }
+}
+
+
+
+
+
+
+
+  
+
+
+
+  
