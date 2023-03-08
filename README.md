42f0211d3630ebf0c0fca683b30c5b466f9874e0
Corentin Miserque <corentinmiserque1@gmail.com>
2023-03-08 15:10:36 +0100

    modif

--- README.md
+++ README.md
@@ -1,11 +1,25 @@
-# collection-project
-# TECHNOLOGIE:
-* JS
-* SCSS
-* HTML
+## Badges
 
-# Author:
- Corentin Miserque
+[![MIT License](https://img.shields.io/badge/-HTML-lightgrey)](https://choosealicense.com/licenses/mit/)
+[![MIT License](https://img.shields.io/badge/-SCSS-blue)](https://choosealicense.com/licenses/mit/)
+[![MIT License](https://img.shields.io/badge/-JavaScript-yellow)](https://choosealicense.com/licenses/mit/)
 
-# LINK:
-Github page:https://corentinmiserque.github.io/collection-project1/
+# Collection project
+
+### Welcome to my repository.
+
+This repository was created in order to fullfill a request made by one of our coaches during our training. It is a collection of items based on a javascript object.
+
+## Tech Stack
+
+**Client:** HTML, SCSS; JavaScript
+
+## Authors
+
+- Corentin Miserque
+
+## Screenshots
+
+![mobile](./asset/mobile.png)
+
+![active](./asset/active.png
--- script.js
+++ script.js
@@ -6,7 +6,7 @@ const collectionSerie = [
         picture: "https://images.critictoo.com/wp-content/uploads/2018/09/Elite-Saison-1.jpg",
         genre: "Suspense",
         summary:"Las Encinas est une école privée huppée d’Espagne réservée aux enfants de l’élite. Mais quand leur lycée est détruit, trois adolescents issus de la classe populaire y sont intégrés. Cependant, ils vont vite regretter cette opportunité... ",
-        logo:"https://upload.wikimedia.org/wikipedia/commons/thumb/5/54/Netflix_logo.png/800px-Netflix_logo.png",
+        logo:"https://cdn-icons-png.flaticon.com/512/732/732228.png",
         link:"https://www.netflix.com/browse",
     },
     {
@@ -16,7 +16,7 @@ const collectionSerie = [
         picture: "https://fr.web.img3.acsta.net/pictures/20/06/18/09/35/0755176.jpg",
         genre: "Action",
         summary:"Une jeune femme d'une vingtaine d'années se réveille à la morgue, vivante, avec un artefact implanté dans le dos. Elle découvre qu'elle est maintenant membre d'un ordre ancien qui s'est donné pour mission de combattre les démons sur Terre.",
-        logo:"https://upload.wikimedia.org/wikipedia/commons/thumb/5/54/Netflix_logo.png/800px-Netflix_logo.png",
+        logo:"https://cdn-icons-png.flaticon.com/512/732/732228.png",
         link:"https://www.netflix.com/browse",
     },
     {
@@ -26,7 +26,7 @@ const collectionSerie = [
         picture: "https://www.bellanaija.com/wp-content/uploads/2020/05/Blood-and-Water-2-750x600.jpeg",
         genre: "Suspense",
         summary:"Une adolescente obtient son transfert dans le même lycée qu'une jeune fille qu'elle soupçonne d'être sa soeur, enlevée à la naissance 17 ans auparavant.",
-        logo:"https://upload.wikimedia.org/wikipedia/commons/thumb/5/54/Netflix_logo.png/800px-Netflix_logo.png",
+        logo:"https://cdn-icons-png.flaticon.com/512/732/732228.png",
         link:"https://www.netflix.com/browse",
     },
     {
@@ -36,7 +36,7 @@ const collectionSerie = [
         picture: "https://fr.web.img5.acsta.net/pictures/18/02/19/15/23/4086911.jpg",
         genre: "Policier",
         summary:"Olivia Moore, surnommée Liv, une étudiante transformée en zombie lors d'une soirée qui a très mal tourné, travaille en tant que médecin légiste afin de pouvoir profiter du festin que représentent pour elle les cervelles des défunts. ",
-        logo:"https://upload.wikimedia.org/wikipedia/commons/thumb/5/54/Netflix_logo.png/800px-Netflix_logo.png",
+        logo:"https://cdn-icons-png.flaticon.com/512/732/732228.png",
         link:"https://www.netflix.com/browse",
     },
     {
@@ -46,7 +46,7 @@ const collectionSerie = [
         picture:"https://www.nautiljon.com/images/drama/00/77/imawa_no_kuni_no_alice_4477.jpg",
         genre: "Fantastique",
         summary:"Un jeune homme passionné de jeux vidéo se retrouve avec ses deux amis dans un Tokyo alternatif où ils doivent disputer de dangereuses parties pour survivre.",
-        logo:"https://upload.wikimedia.org/wikipedia/commons/thumb/5/54/Netflix_logo.png/800px-Netflix_logo.png",
+        logo:"https://cdn-icons-png.flaticon.com/512/732/732228.png",
         link:"https://www.netflix.com/browse",
     },
     {
@@ -56,7 +56,7 @@ const collectionSerie = [
         picture: "https://ichef.bbci.co.uk/images/ic/1200x675/p07p5h4s.jpg",
         genre: "Humoristique",
         summary:"L'émission est un concours de drag queens au cours duquel RuPaul, qui est à la fois présentatrice, mentor et source d'inspiration pour les candidates, sélectionne la « prochaine superstar américaine du drag.",
-        logo:"https://upload.wikimedia.org/wikipedia/commons/thumb/5/54/Netflix_logo.png/800px-Netflix_logo.png",
+        logo:"https://cdn-icons-png.flaticon.com/512/732/732228.png",
         link:"https://www.netflix.com/browse",
     },
     {
@@ -66,7 +66,7 @@ const collectionSerie = [
         picture: "https://flxt.tmsimg.com/assets/p10781465_b_v13_ay.jpg",
         genre: "Super-Hero",
         summary:"Jeune expert de la police scientifique de Central City, Barry Allen se retrouve doté d'une vitesse extraordinaire après avoir été frappé par la foudre. Sous le costume de Flash, il utilise ses nouveaux pouvoirs pour combattre le crime.",
-        logo:"https://upload.wikimedia.org/wikipedia/commons/thumb/5/54/Netflix_logo.png/800px-Netflix_logo.png",
+        logo:"https://cdn-icons-png.flaticon.com/512/732/732228.png",
         link:"https://www.netflix.com/browse",
     },
     {
@@ -76,7 +76,7 @@ const collectionSerie = [
         picture: "https://fr.web.img5.acsta.net/pictures/17/08/30/11/50/532007.jpg",
         genre: "Thriller",
         summary:"Le criminel le plus recherché du monde se rend mystérieusement à la police et offre ses services en dénonçant tous ceux avec qui il a collaboré sur ses crimes par le passé. Sa seule contrepartie : travailler avec une agent du FBI débutante avec qui il n'a semble-t-il aucune connexion...",
-        logo:"https://upload.wikimedia.org/wikipedia/commons/thumb/5/54/Netflix_logo.png/800px-Netflix_logo.png",
+        logo:"https://cdn-icons-png.flaticon.com/512/732/732228.png",
         link:"https://www.netflix.com/browse",
     },
     {
@@ -86,7 +86,7 @@ const collectionSerie = [
         picture: "https://fr.web.img2.acsta.net/pictures/20/02/25/06/36/4522700.jpg",
         genre: "Comédie",
         summary:"Deux amis inséparables au lycée se retrouvent à New York après s'être perdus de vue, mais leur nouveau terrain de jeu est radicalement différent.",
-        logo:"https://upload.wikimedia.org/wikipedia/commons/thumb/5/54/Netflix_logo.png/800px-Netflix_logo.png",
+        logo:"https://cdn-icons-png.flaticon.com/512/732/732228.png",
         link:"https://www.netflix.com/browse",
     },
     {
@@ -96,7 +96,7 @@ const collectionSerie = [
         picture: "https://fr.web.img6.acsta.net/pictures/18/08/14/16/37/4771722.jpg",
         genre: "Comédie",
         summary:"Les drôles de mésaventures des employés d'un grand magasin. Des débutants aux yeux qui brillent aux vétérans ayant déjà tout vécu, en passant par les employés d'été paumés et les managers déterminés, ils affrontent 'en famille' les soucis du quotidien inhérents à la gestion d'une grande surface.",
-        logo:"https://upload.wikimedia.org/wikipedia/commons/thumb/5/54/Netflix_logo.png/800px-Netflix_logo.png",
+        logo:"https://cdn-icons-png.flaticon.com/512/732/732228.png",
         link:"https://www.netflix.com/browse",
     },
     {
@@ -106,7 +106,7 @@ const collectionSerie = [
         picture: "https://fr.web.img5.acsta.net/pictures/18/04/10/10/27/5800369.jpg",
         genre: "Super Hero",
         summary:"Les aventures de Supergirl. A 24 ans, elle mène sa vie en marge de celle de son célèbre cousin, se sent impuissante face à ses pouvoirs extraordinaires qu'on lui a appris à ne pas utiliser. Elle va se retrouver à mener une double vie pour combattre le crime dans sa ville.",
-        logo:"https://upload.wikimedia.org/wikipedia/commons/thumb/5/54/Netflix_logo.png/800px-Netflix_logo.png",
+        logo:"https://cdn-icons-png.flaticon.com/512/732/732228.png",
         link:"https://www.netflix.com/browse",
     },
     {
@@ -116,7 +116,7 @@ const collectionSerie = [
         picture: "https://images.justwatch.com/poster/97646440/s592/cest-du-gateau",
         genre: "Humoristique",
         summary:"De piètres pâtissiers amateurs tentent de reproduire des pâtisseries à l'esthétique complexe, dont la réalisation tient souvent plus de la sculpture que de la simple pâtisserie. Une mission quasi-impossible pour les candidats..",
-        logo:"https://upload.wikimedia.org/wikipedia/commons/thumb/5/54/Netflix_logo.png/800px-Netflix_logo.png",
+        logo:"https://cdn-icons-png.flaticon.com/512/732/732228.png",
         link:"https://www.netflix.com/browse",
     },
 ]
@@ -156,7 +156,7 @@ newbody.appendChild(parent);
         let logoimg = document.createElement("img");
         logoimg.classList.add("logo");
         divNumber.appendChild(logoimg);
-        logoimg.setAttribute("src","https://upload.wikimedia.org/wikipedia/commons/thumb/5/54/Netflix_logo.png/800px-Netflix_logo.png");
+        logoimg.setAttribute("src","https://cdn-icons-png.flaticon.com/512/732/732228.png");
     
 //creation div invisible
 
